c4418519182e3b99e0dd41cfa249c6f11e3d87c4
Julien Batonnet <julien.batonnet@gmail.com>
2019-04-27 23:18:48 -0400

    Base structure (Asp.Net core, Blazor, console, shared)

---
+++ Inbox.Console/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Inbox.Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
